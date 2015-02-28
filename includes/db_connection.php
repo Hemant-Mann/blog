2a6fc4159949bc93670e42163c69ee561efbb8ab
Hemant-Mann <hemant.mann121@gmail.com>
2015-02-28 19:20:27 +0530

    db credentials

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "fhemzrant");
-define("DB_NAME", "blog");
+define("DB_USER", ""); // your username
+define("DB_PASS", ""); // your password
+define("DB_NAME", "blog");	// database name
 //creates database connection
  
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
